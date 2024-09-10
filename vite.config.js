e06527101ea11f2a16abf21fce85ddedde41e6ff
marder <marcin.derucki@gmail.com>
2024-09-10 18:45:13 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://xder.bieda.it/blog",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
